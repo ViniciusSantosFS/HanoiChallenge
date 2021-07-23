bd0bf24ee24818612881df1e8dc89713f1b08378
vinicius santos <viniciussantos@pop-os.localdomain>
2021-07-23 09:47:55 -0300

    Hanoi recursivo

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
-const pino1 = [3, 2, 1];
-const pino2 = [];
-const pino3 = [];
 let jogadas = 0;
-let total = pino1.length;
 
-while (pino3.length < total) {
-  if (
-    pino1[0] !== undefined &&
-    pino3[0] !== undefined &&
-    pino2[0] !== undefined &&
-    pino3[pino3.length - 1] > pino2[pino2.length - 1]
-  ) {
-    pino3.push(pino2[pino2.length - 1]);
-    pino2.pop();
-    jogadas++;
+const move = (origem, destino) => {
+  console.log(`mova de ${origem} -> ${destino}`);
+};
+
+const hanoi = (n, origem, destino, aux) => {
+  jogadas++;
+  if (n == 1) {
+    move(origem, destino);
+    return;
   } else {
-    if (pino3[0] === undefined) {
-      pino3.push(pino1[pino1.length - 1]);
-      pino1.pop();
-      jogadas++;
-    } else if (
-      pino1[0] !== undefined &&
-      pino3[pino3.length - 1] > pino1[pino1.length - 1]
-    ) {
-      pino3.push(pino1[pino1.length - 1]);
-      pino1.pop();
-      jogadas++;
-    } else if (pino2[0] === undefined) {
-      pino2.push(pino1[pino1.length - 1]);
-      pino1.pop();
-      jogadas++;
-    } else if (
-      pino1[0] !== undefined &&
-      pino2[pino2.length - 1] > pino1[pino1.length - 1]
-    ) {
-      pino2.push(pino1[pino1.length - 1]);
-      pino1.pop();
-      jogadas++;
-    } else if (pino2[pino2.length - 1] > pino3[pino3.length - 1]) {
-      pino2.push(pino3[pino3.length - 1]);
-      pino3.pop();
-      jogadas++;
-    } else if (pino1[0] === undefined) {
-      pino1.push(pino2[pino2.length - 1]);
-      pino2.pop();
-      jogadas++;
-    }
+    hanoi(n - 1, origem, aux, destino);
+    move(origem, destino);
+    hanoi(n - 1, aux, destino, origem);
   }
-}
+};
 
-console.log(`PINO: ${pino3} Número minimo de jogadas: ${jogadas}`);
+hanoi(3, "A", "B", "C");
+console.log("JOGADAS: " + jogadas);
